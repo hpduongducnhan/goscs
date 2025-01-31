d388429e575cfe3591c5cf22ad839437aeb4b679
NhanDD <hp.duongducnhan@gmail.com>
2025-01-31 13:50:17 +0700

    New logger

--- logger.go
+++ logger.go
@@ -2,42 +2,33 @@ package goscs
 
 import (
 	"os"
+	"strings"
 
 	"github.com/rs/zerolog"
-	"github.com/rs/zerolog/log"
-	"github.com/rs/zerolog/pkgerrors"
 )
 
-func ConfigureLogger(debug bool) {
-	// zerolog allows for logging at the following levels (from highest to lowest):
+var logger zerolog.Logger
+var isDev bool = strings.ToLower(os.Getenv("dev")) == "true" || os.Getenv("dev") == "1"
 
-	// panic (zerolog.PanicLevel, 5)
-	// fatal (zerolog.FatalLevel, 4)
-	// error (zerolog.ErrorLevel, 3)
-	// warn (zerolog.WarnLevel, 2)
-	// info (zerolog.InfoLevel, 1)
-	// debug (zerolog.DebugLevel, 0)
-	// trace (zerolog.TraceLevel, -1)
+func init() {
+	configureLogger("default", zerolog.InfoLevel)
+}
 
-	// configure log time format
-	zerolog.TimeFieldFormat = zerolog.TimeFormatUnix
-	zerolog.ErrorStackMarshaler = pkgerrors.MarshalStack
-
-	zerolog.SetGlobalLevel(zerolog.InfoLevel)
-	if debug {
-		zerolog.SetGlobalLevel(zerolog.DebugLevel)
+func configureLogger(name string, level zerolog.Level) {
+	zerolog.SetGlobalLevel(level)
+	if isDev {
+		logger = zerolog.New(zerolog.ConsoleWriter{Out: os.Stdout}).With().Str("module", name).Timestamp().Caller().Logger()
+	} else {
+		logger = zerolog.New(os.Stdout).With().Str("module", name).Timestamp().Caller().Logger()
 	}
+}
 
-	// log.Debug().
-	// 	Str("Scale", "833 cents").
-	// 	Float64("Interval", 833.09).
-	// 	Msg("Fibonacci is everywhere")
-
-	// log.Debug().
-	// 	Str("Name", "Tom").
-	// 	Send()
-
-	// pretty print
-	log.Logger = log.Output(zerolog.ConsoleWriter{Out: os.Stderr})
-
+func GetLoger(name string, level zerolog.Level) zerolog.Logger {
+	if name == "" || level == 0 {
+		// default logger
+		return logger
+	} else {
+		configureLogger(name, level)
+		return logger
+	}
 }
